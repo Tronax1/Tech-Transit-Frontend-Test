0d74d480eaaf123e15db55c982254a10bcf87bba
Gongo <jvilla1714@gmail.com>
2020-01-19 09:40:33 -0500

    Added structure for the card

--- client/src/components/Courses/CourseCard.js
+++ client/src/components/Courses/CourseCard.js
@@ -4,8 +4,16 @@ import '../../styles/CourseCard.scss'
 export default function CourseCard() {
     return (
         <div className = "card-bg">
-            <h3>The title</h3>
-            <h5>The author</h5>          
+            <div className = "card-header">
+
+            </div>
+            <div className = "card-body">
+                <h3>The title</h3>
+                <h5>The author</h5>
+            </div>
+            <div className = "card-footer">
+
+            </div>    
         </div>
     )
 }
