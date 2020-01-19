b0f4bbb8a85847b46fbdf7d47209fb6ef38b4bd3
Gongo <jvilla1714@gmail.com>
2020-01-18 19:06:51 -0500

    Added function to dinamically render the courses

--- client/src/components/Course Page/CoursePage.js
+++ client/src/components/Course Page/CoursePage.js
@@ -5,27 +5,23 @@ import {useState, useEffect} from 'react'
 
 import '../../styles/CoursePage.scss'
 
-export default function CoursePage(props) {
-    const [courses, setCourses] = useState('');
+export default function CoursePage() {
+    const [courses, setCourses] = useState([]);
 
     useEffect(()=>{
         async function fetchData() {
             const allCourses = await axios.get('https://cors-anywhere.herokuapp.com/https://test.mytablemesa.com/api/courses?orderBy=popularity+desc&expand=provider&limit=24&profession=&subjectAreaCode=&state=&provider=&name=')
-            setCourses(allCourses);
+            setCourses(allCourses.data.items);
         }
         fetchData();
-    });
+    }, []);
     console.log(courses);
 
     return (
         <div className = "Courses-Structure">
-            <CourseCard/> 
-            <CourseCard /> 
-            <CourseCard /> 
-            <CourseCard /> 
-            <CourseCard /> 
-            <CourseCard /> 
-            <CourseCard />           
+            {courses.map((item, i) =>(
+                <CourseCard key={i} />
+            ))}
         </div>
     )
 }
